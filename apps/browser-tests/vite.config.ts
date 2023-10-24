7a730690627f1433186febdf5f283839723b89c1
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-10-24 15:55:37 +0800

    fix(sdk): polyfill buffer for test app

---
+++ apps/browser-tests/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+export default defineConfig({
+  plugins: [
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+      },
+    }),
+  ],
+});
