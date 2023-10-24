0f3f4e4759f11a6881e05f5b2febc0cdec4b1bf2
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-10-24 15:32:07 +0800

    refactor(sdk): cleanup addresses

--- packages/sdk/src/addresses/formats.ts
+++ packages/sdk/src/addresses/formats.ts
@@ -1,3 +1,22 @@
+export const ADDRESS_TYPE_TO_NAME = {
+  p2pkh: "legacy",
+  p2sh: "nested-segwit",
+  p2wpkh: "segwit",
+  p2tr: "taproot",
+} as const;
+
+export const ADDRESS_NAME_TO_TYPE = {
+  legacy: "p2pkh",
+  "nested-segwit": "p2sh",
+  segwit: "p2wpkh",
+  taproot: "p2tr",
+} as const;
+
+export type AddressTypes = keyof typeof ADDRESS_TYPE_TO_NAME;
+export type AddressFormats =
+  | (typeof ADDRESS_TYPE_TO_NAME)[AddressTypes]
+  | "unknown";
+
 export const ADDRESS_FORMATS = {
   mainnet: {
     p2pkh: /^[1][a-km-zA-HJ-NP-Z1-9]{25,34}$/,
@@ -18,22 +37,3 @@ export const ADDRESS_FORMATS = {
     p2tr: /^(tb1p|bcrt1p)[a-zA-HJ-NP-Z0-9]{14,74}$/,
   },
 } as const;
-
-export const ADDRESS_TYPE_TO_NAME = {
-  p2pkh: "legacy",
-  p2sh: "nested-segwit",
-  p2wpkh: "segwit",
-  p2tr: "taproot",
-} as const;
-
-export const ADDRESS_NAME_TO_TYPE = {
-  legacy: "p2pkh",
-  "nested-segwit": "p2sh",
-  segwit: "p2wpkh",
-  taproot: "p2tr",
-} as const;
-
-export type AddressTypes = keyof typeof ADDRESS_TYPE_TO_NAME;
-export type AddressFormats =
-  | (typeof ADDRESS_TYPE_TO_NAME)[AddressTypes]
-  | "unknown";
--- packages/sdk/src/addresses/index.ts
+++ packages/sdk/src/addresses/index.ts
@@ -1,31 +1,15 @@
-// import * as ecc from "@bitcoinerlab/secp256k1";
-// import BIP32Factory, { BIP32Interface } from "bip32";
-
-import { Network } from "../networks/types";
-// import {
-//   createTransaction,
-//   getDerivationPath,
-//   getNetwork,
-//   toXOnly,
-// } from "../utils";
 import {
-  AddressFormats,
   ADDRESS_FORMATS,
   AddressTypes,
   ADDRESS_TYPE_TO_NAME,
+  AddressFormats,
 } from "./formats";
+import type { Network } from "../networks/types";
 
-type AddressFormat = {
-  address: string;
-  format: AddressFormats;
-};
-
-export function getAddressFormat(address: string, network: Network) {
-  let format: AddressFormat = {
-    address,
-    format: "unknown",
-  };
-
+export function getAddressFormat(
+  address: string,
+  network: Network,
+): AddressFormats {
   const addressTypes = ADDRESS_FORMATS[network];
   const addressTypesList = Object.keys(addressTypes);
 
@@ -35,201 +19,9 @@ export function getAddressFormat(address: string, network: Network) {
     const addressName = ADDRESS_TYPE_TO_NAME[addressType];
 
     if (addressTypeRegex.test(address)) {
-      format = {
-        address,
-        format: addressName,
-      };
+      return addressName;
     }
   }
 
-  return format;
+  return "unknown";
 }
-
-// export function getAddressType(
-//   address: string,
-//   network: Network,
-// ): AddressTypes {
-//   const addressFormat = getAddressFormat(address, network).format;
-//   return ADDRESS_NAME_TO_TYPE[addressFormat as AddressFormats];
-// }
-
-/*
-export function getAddressesFromPublicKey(
-  pubKey: string | Buffer,
-  network: Network = "testnet",
-  format: AddressTypes | "all" = "all",
-) {
-  if (!Buffer.isBuffer(pubKey)) {
-    pubKey = Buffer.from(pubKey, "hex");
-  }
-  const bip32 = BIP32Factory(ecc);
-  const networkObj = getNetwork(network);
-  const chainCode = Buffer.alloc(32).fill(1);
-
-  const addresses: Address[] = [];
-
-  let childNodeXOnlyPubkey = pubKey;
-
-  const keys = bip32.fromPublicKey(pubKey, chainCode, networkObj);
-
-  childNodeXOnlyPubkey = keys.publicKey.subarray(1, 33);
-
-  if (format === "all") {
-    const addressTypesList = Object.keys(
-      ADDRESS_TYPE_TO_NAME,
-    ) as AddressTypes[];
-
-    addressTypesList.forEach((addrType) => {
-      if (addrType === "p2tr") {
-        const paymentObj = createTransaction(
-          childNodeXOnlyPubkey,
-          addrType,
-          network,
-        );
-
-        addresses.push({
-          address: paymentObj.address,
-          xkey: childNodeXOnlyPubkey.toString("hex"),
-          format: ADDRESS_TYPE_TO_NAME[addrType],
-          pub: keys.publicKey.toString("hex"),
-        });
-      } else {
-        const paymentObj = createTransaction(keys.publicKey, addrType, network);
-
-        addresses.push({
-          address: paymentObj.address,
-          format: ADDRESS_TYPE_TO_NAME[addrType],
-          pub: keys.publicKey.toString("hex"),
-        });
-      }
-    });
-  } else {
-    const key = format === "p2tr" ? childNodeXOnlyPubkey : keys.publicKey;
-    const paymentObj = createTransaction(key, format, network);
-
-    addresses.push({
-      address: paymentObj.address,
-      format: ADDRESS_TYPE_TO_NAME[format],
-      pub: keys.publicKey.toString("hex"),
-      xkey:
-        format === "p2tr" ? childNodeXOnlyPubkey.toString("hex") : undefined,
-    });
-  }
-
-  return addresses;
-}
-
-export async function getAddresses({
-  pubKey,
-  network,
-  format,
-}: GetAddressesOptions): Promise<ReturnType<typeof getAddressesFromPublicKey>> {
-  return getAddressesFromPublicKey(pubKey, network, format);
-}
-
-export function getAccountDataFromHdNode({
-  hdNode,
-  format = "legacy",
-  network = "testnet",
-  account = 0,
-  addressIndex = 0,
-}: GetAccountDataFromHdNodeOptions) {
-  if (!hdNode) {
-    throw new Error("Invalid options provided.");
-  }
-
-  const addressType = ADDRESS_TYPE_TO_NAME[format];
-
-  const fullDerivationPath = getDerivationPath(format, account, addressIndex);
-  const child = hdNode.derivePath(fullDerivationPath);
-
-  const pubKey =
-    format === "taproot" ? toXOnly(child.publicKey) : child.publicKey;
-  const paymentObj = createTransaction(pubKey, addressType, network);
-
-  const address = paymentObj.address!;
-
-  const accountData: Account = {
-    address,
-    pub: child.publicKey.toString("hex"),
-    priv: child.privateKey!.toString("hex"),
-    format,
-    type: addressType,
-    derivationPath: {
-      account,
-      addressIndex,
-      path: fullDerivationPath,
-    },
-    child,
-  };
-
-  if (format === "taproot") {
-    accountData.xkey = toXOnly(child.publicKey).toString("hex");
-  }
-
-  return accountData;
-}
-
-export function getAllAccountsFromHdNode({
-  hdNode,
-  network = "testnet",
-}: GetAllAccountsFromHDNodeOptions) {
-  const accounts: Account[] = [];
-  const addressTypesList = Object.values(
-    ADDRESS_TYPE_TO_NAME,
-  ) as AddressFormats[];
-
-  addressTypesList.forEach((addrType) => {
-    const account = getAccountDataFromHdNode({
-      hdNode,
-      format: addrType,
-      network,
-    });
-
-    accounts.push(account);
-  });
-
-  return accounts;
-}
-
-export type Address = {
-  address: string | undefined;
-  xkey?: string;
-  format: string;
-  pub: string;
-};
-
-export type Derivation = {
-  account: number;
-  addressIndex: number;
-  path: string;
-};
-
-export type Account = Address & {
-  priv: string;
-  type: AddressTypes;
-  derivationPath: Derivation;
-  child: BIP32Interface;
-};
-
-type GetAddressesOptions = {
-  pubKey: string;
-  network: Network;
-  format: AddressTypes | "all";
-};
-
-type GetAccountDataFromHdNodeOptions = {
-  hdNode: BIP32Interface;
-  format?: AddressFormats;
-  network?: Network;
-  account?: number;
-  addressIndex?: number;
-};
-
-type GetAllAccountsFromHDNodeOptions = Omit<
-  GetAccountDataFromHdNodeOptions,
-  "format"
->;
-
-export * from "./formats";
-*/
