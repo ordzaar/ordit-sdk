dd322129143ed5bf396b377090db63ea5b309a90
Daniel <daniel@adip.me>
2023-10-24 16:10:13 +0800

    Code formatting

--- apps/website/docusaurus.config.js
+++ apps/website/docusaurus.config.js
@@ -1,51 +1,51 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Ordit SDK',
-  tagline: 'A powerful Bitcoin Ordinals-aware library written in TypeScript',
-  favicon: 'img/favicon.ico',
+  title: "Ordit SDK",
+  tagline: "A powerful Bitcoin Ordinals-aware library written in TypeScript",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: "https://your-docusaurus-test-site.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ordzaar', // Usually your GitHub org/user name.
-  projectName: 'ordit-sdk', // Usually your repo name.
+  organizationName: "ordzaar", // Usually your GitHub org/user name.
+  projectName: "ordit-sdk", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -55,62 +55,62 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Ordit SDK',
+        title: "Ordit SDK",
         logo: {
-          alt: 'Ordzaar Logo',
-          src: 'img/ordzaar-256x256.png',
+          alt: "Ordzaar Logo",
+          src: "img/ordzaar-256x256.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Getting Started',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Getting Started",
           },
           {
-            href: 'https://github.com/ordzaar/ordit-sdk',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/ordzaar/ordit-sdk",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Getting Started',
-                to: '/docs/intro',
+                label: "Getting Started",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.com/invite/ordzaar',
+                label: "Discord",
+                href: "https://discord.com/invite/ordzaar",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/ordzaar',
+                label: "Twitter",
+                href: "https://twitter.com/ordzaar",
               },
               {
-                label: 'Medium',
-                href: 'https://blog.ordzaar.com/',
+                label: "Medium",
+                href: "https://blog.ordzaar.com/",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/ordzaar/ordit-sdk',
+                label: "GitHub",
+                href: "https://github.com/ordzaar/ordit-sdk",
               },
             ],
           },
