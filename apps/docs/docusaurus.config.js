f87caad831ef21043392624c3c9707b4605c9e66
Daniel Adipranoto <daniel.adipranoto@birthday.dev>
2023-10-25 18:16:28 +0800

    Make intro page the landing page

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -38,12 +38,14 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
+        blog: false,
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
