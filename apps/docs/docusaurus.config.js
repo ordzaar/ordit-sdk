24efb33fecedf74eeb169bde0bae498ed76c08b3
Daniel Adipranoto <daniel.adipranoto@birthday.dev>
2023-10-25 18:22:33 +0800

    Remove unused static files

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/ordzaar-logo.gif",
       navbar: {
         title: "Ordit SDK",
         logo: {
