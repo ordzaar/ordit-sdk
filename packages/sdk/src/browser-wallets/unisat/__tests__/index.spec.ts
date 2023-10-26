e6be24058782f545620e2b63fb89bcd79ba10257
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-10-26 11:05:22 +0800

    test(sdk): add tests for getAddresses

--- packages/sdk/src/browser-wallets/unisat/__tests__/index.spec.ts
+++ packages/sdk/src/browser-wallets/unisat/__tests__/index.spec.ts
@@ -1,12 +1,13 @@
 // @vitest-environment happy-dom
-import { isInstalled } from "..";
+import { getAddresses, isInstalled } from "..";
+import { NETWORK_TO_UNISAT_NETWORK } from "../constants";
 
 describe("Unisat Wallet", () => {
-  describe("isInstalled", () => {
-    afterEach(() => {
-      vi.unstubAllGlobals();
-    });
+  afterEach(() => {
+    vi.unstubAllGlobals();
+  });
 
+  describe("isInstalled", () => {
     test("should return true if installed", () => {
       vi.stubGlobal("unisat", {});
       expect(typeof window).not.toBeUndefined();
@@ -16,6 +17,40 @@ describe("Unisat Wallet", () => {
     test("should return false if not installed", () => {
       expect(typeof window).not.toBeUndefined();
       expect(isInstalled()).toBeFalsy();
+    });
+  });
+
+  describe("getAddresses", () => {
+    test("should return address from mainnet", () => {
+      const mockData = {
+        pub: "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
+        address: "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4",
+        format: "segwit",
+      };
+      const network = "mainnet";
+
+      vi.stubGlobal("unisat", {
+        getNetwork: vi.fn(() => NETWORK_TO_UNISAT_NETWORK[network]),
+        requestAccounts: vi.fn(() => [mockData.address]),
+        getPublicKey: vi.fn(() => mockData.pub),
+      });
+      expect(getAddresses("mainnet")).resolves.toEqual([mockData]);
+    });
+
+    test("should return address from testnet", () => {
+      const mockData = {
+        pub: "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
+        address: "tb1qw508d6qejxtdg4y5r3zarvary0c5xw7kxpjzsx",
+        format: "segwit",
+      };
+      const network = "testnet";
+
+      vi.stubGlobal("unisat", {
+        getNetwork: vi.fn(() => NETWORK_TO_UNISAT_NETWORK[network]),
+        requestAccounts: vi.fn(() => [mockData.address]),
+        getPublicKey: vi.fn(() => mockData.pub),
+      });
+      expect(getAddresses("testnet")).resolves.toEqual([mockData]);
     });
   });
 });
