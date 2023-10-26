bb5325da95c5d7918d3990969d02b0144d5684e3
Daniel <daniel@adip.me>
2023-10-26 14:09:00 +0800

    Remove unused code and assets

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -42,8 +42,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/ordzaar/ordit-sdk/apps/docs/",
         },
         blog: false,
         theme: {
