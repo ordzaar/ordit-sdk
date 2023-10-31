96fc3bf7256239e213415e5f47a5d4c6e4b40ed7
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-10-31 15:57:28 +0800

    chore(sdk): replace comment to JSDoc compatible comment

--- packages/sdk/src/constants/index.ts
+++ packages/sdk/src/constants/index.ts
@@ -1,9 +1,15 @@
-// amount lower than this is considered as dust value
-// and majority of the miners don't pick txs w/ the following output value or lower
+/**
+ * Amount lower than this is considered as dust value
+ * and majority of the miners don't pick txs w/ the following output value or lower
+ */
 export const MINIMUM_AMOUNT_IN_SATS = 600;
 
-// Fee calculated by the fee estimator cannot be greater than 0.05 BTC in any case
+/**
+ * Fee calculated by the fee estimator cannot be greater than 0.05 BTC in any case
+ */
 export const MAXIMUM_FEE = 5000000;
 
-// Maximum number of bytes pushable to the witness stack
+/**
+ * Maximum number of bytes pushable to the witness stack
+ */
 export const MAXIMUM_SCRIPT_ELEMENT_SIZE = 520;
