3163c2164665c008b75dc6c3b7dc6891dd190ff5
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-11-01 13:55:30 +0800

    chore(ops): update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "./node_modules/@waveshq/standard-prettier",
   ],
+  plugins: ["simple-import-sort", "import"],
   parser: "@typescript-eslint/parser",
   rules: {
     "object-shorthand": 2,
@@ -18,5 +19,26 @@ module.exports = {
         caughtErrorsIgnorePattern: "^_",
       },
     ],
+    "@typescript-eslint/no-floating-promises": 2,
+    "no-shadow": 2,
+    "no-param-reassign": 2,
+    "no-unneeded-ternary": 2,
+    "require-await": 2,
+    "simple-import-sort/imports": 2,
+    "simple-import-sort/exports": 2,
+    "import/first": 2,
+    "import/newline-after-import": 2,
+    "import/no-duplicates": 2,
+    "no-nested-ternary": 2,
+    "no-unused-expressions": 2,
+    "no-return-assign": 2,
   },
+  overrides: [
+    {
+      files: ["**/src/**/**.spec.ts"],
+      rules: {
+        "@typescript-eslint/no-floating-promises": 0,
+      },
+    },
+  ],
 };
