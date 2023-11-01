63aad46589391841e63b9ad23083d222f0942771
JJ Adonis <work@josejai.me>
2023-11-01 15:39:12 +0800

    chore(ops): move to @ordzaar/standard (#30)
    * chore(ops): move to @ordzaar/standard
    
    * chore(ops): move to @ordzaar/standard
    
    * chore(ops): move to @ordzaar/standard
    
    * chore(ops): move to @ordzaar/standard
    
    * chore(ops): move to @ordzaar/standard
    
    * chore(ops): move to @ordzaar/standard
    
    * chore(ops): move to @ordzaar/standard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "./node_modules/@waveshq/standard-prettier",
+    "./node_modules/@ordzaar/standard-prettier",
   ],
   parser: "@typescript-eslint/parser",
   rules: {
