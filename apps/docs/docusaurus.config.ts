0b87287d58ae639a8a3177d0e3e5814c752306a5
JJ Adonis <work@josejai.me>
2023-11-02 00:56:06 +0800

    chore(ops): update docusaurus (#32)
    * chore(ops): update ordit
    
    * Fix issue with prism react renderer
    
    * Migrate config from js to ts
    
    * Remove redundant comments
    
    ---------
    
    Co-authored-by: Daniel Adipranoto <daniel.adipranoto@birthday.dev>

---
+++ apps/docs/docusaurus.config.ts
@@ -0,0 +1,122 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Ordit SDK",
+  tagline: "A powerful Bitcoin Ordinals-aware library written in TypeScript",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-test-site.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "ordzaar", // Usually your GitHub org/user name.
+  projectName: "ordit-sdk", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/ordzaar/ordit-sdk/apps/docs/",
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/ordzaar-logo.gif",
+    navbar: {
+      title: "Ordit SDK",
+      logo: {
+        alt: "Ordzaar Logo",
+        src: "img/ordzaar-256x256.png",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          href: "https://github.com/ordzaar/ordit-sdk",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Docs",
+              to: "/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.com/invite/ordzaar",
+            },
+            {
+              label: "Twitter",
+              href: "https://x.com/ordzaar",
+            },
+            {
+              label: "Medium",
+              href: "https://blog.ordzaar.com/",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/ordzaar/ordit-sdk",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Ordzaar.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
+};
+
+export default config;
