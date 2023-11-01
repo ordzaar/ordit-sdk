7fd2566820670094b252c7821707818b386989f6
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-11-01 17:48:57 +0800

    test(sdk): update FeeEstimator test

--- packages/sdk/src/fee/__tests__/FeeEstimator.spec.ts
+++ packages/sdk/src/fee/__tests__/FeeEstimator.spec.ts
@@ -1,13 +1,49 @@
+import { OrditSDKError } from "../../errors";
 import { FeeEstimator } from "../FeeEstimator";
 import { createMockPsbt } from "../utils";
 
 describe("FeeEstimator", () => {
-  test("should return an estimated network fee for P2SH-P2WPKH psbt when calculateNetworkFee is called", () => {
-    const feeEstimator = new FeeEstimator({
-      feeRate: 1,
-      network: "mainnet",
-      psbt: createMockPsbt("p2sh-p2wpkh"),
+  describe("constructor", () => {
+    const INVALID_FEE_RATE_ERROR = new OrditSDKError("Invalid feeRate");
+    test("should throw an error for negative fee rate", () => {
+      expect(() => {
+        new FeeEstimator({
+          feeRate: -1,
+          network: "mainnet",
+          psbt: createMockPsbt("p2sh-p2wpkh"),
+        });
+      }).toThrowError(INVALID_FEE_RATE_ERROR);
     });
-    expect(feeEstimator.calculateNetworkFee()).toBe(255);
+
+    test("should throw an error for fee rates with decimals", () => {
+      expect(() => {
+        new FeeEstimator({
+          feeRate: 1.1,
+          network: "mainnet",
+          psbt: createMockPsbt("p2sh-p2wpkh"),
+        });
+      }).toThrowError(INVALID_FEE_RATE_ERROR);
+    });
+  });
+
+  describe("calculateNetworkFee", () => {
+    test("should return estimated network fee for P2SH-P2WPKH psbt", () => {
+      const feeEstimator = new FeeEstimator({
+        feeRate: 1,
+        network: "mainnet",
+        psbt: createMockPsbt("p2sh-p2wpkh"),
+      });
+      expect(feeEstimator.calculateNetworkFee()).toBe(255);
+    });
+
+    test("should return an estimated network fee for P2SH-P2WPKH psbt with a feeRate that would lose precision", () => {
+      const feeEstimator = new FeeEstimator({
+        // eslint-disable-next-line @typescript-eslint/no-loss-of-precision
+        feeRate: 1.000000000000000001,
+        network: "mainnet",
+        psbt: createMockPsbt("p2sh-p2wpkh"),
+      });
+      expect(feeEstimator.calculateNetworkFee()).toBe(255);
+    });
   });
 });
