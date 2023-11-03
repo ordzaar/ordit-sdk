35beefbfe23394102e92a4f1bf12116a9ea17d66
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-11-03 14:01:11 +0800

    refactor(sdk): remove orderbook and ipfs

--- packages/sdk/src/config/index.ts
+++ packages/sdk/src/config/index.ts
@@ -3,18 +3,12 @@ export const apiConfig = {
   apis: {
     mainnet: {
       batter: "https://mainnet.ordit.io/",
-      orderbook: "1H4vvBnr62YWQmvNSt8Z4pDw3Vcv1n5xz7",
-      ipfs: "http://ipfs-gateway.ordit.io/",
     },
     regtest: {
       batter: "https://regtest.ordit.io/",
-      orderbook: "bcrt1q2ys7qws8g072dqe3psp92pqz93ac6wmztexkh5",
-      ipfs: "http://ipfs-gateway.ordit.io/",
     },
     testnet: {
       batter: "https://testnet.ordit.io/",
-      orderbook: "tb1qfnw26753j7kqu3q099sd48htvtk5wm4e0enmru",
-      ipfs: "http://ipfs-gateway.ordit.io/",
     },
   },
 };
