4c435458110ad2754f8efe8e8f4c8a7cd4398368
Daniel <daniel@adip.me>
2023-11-10 02:02:48 +0800

    Add new errors for extracting tx from non-finalized psbt

--- packages/sdk/src/errors/index.ts
+++ packages/sdk/src/errors/index.ts
@@ -1,3 +1,4 @@
+export * from "./BrowserWalletExtractTxFromNonFinalizedPsbtError";
 export * from "./BrowserWalletNotInstalledError";
 export * from "./BrowserWalletSigningError";
 export * from "./OrditSDKError";
