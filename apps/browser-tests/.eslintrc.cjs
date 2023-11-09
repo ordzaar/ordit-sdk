8b8f6fe4f125397362f17a446547b340fb2a3fa7
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-11-09 18:58:52 +0800

    fix(eslint): turn off console warn for test

--- apps/browser-tests/.eslintrc.cjs
+++ apps/browser-tests/.eslintrc.cjs
@@ -3,6 +3,6 @@ const path = require("path");
 module.exports = {
   parserOptions: { project: [path.join(__dirname, "tsconfig.json")] },
   rules: {
-    "no-console": "warn",
+    "no-console": "off",
   },
 };
