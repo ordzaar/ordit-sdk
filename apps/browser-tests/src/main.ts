54c8205de2991161983a173dbd4396dc996cc230
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-11-09 11:32:01 +0800

    fix(sdk): export PSBTBuilder and createPsbt

--- apps/browser-tests/src/main.ts
+++ apps/browser-tests/src/main.ts
@@ -1,3 +1,4 @@
+import { createPsbt } from "@ordzaar/ordit-sdk";
 import {
   getAddresses as getUnisatAddresses,
   isInstalled as isUnisatInstalled,
@@ -15,4 +16,26 @@ async function connectToUnisat() {
 const unisatConnectButton = document.getElementById("unisat-connect");
 if (unisatConnectButton) {
   unisatConnectButton.addEventListener("click", connectToUnisat);
+}
+
+async function handleCreatePsbtButtonClick() {
+  const psbt = await createPsbt({
+    address: "tb1p98dv6f5jp5qr4z2dtaljvwrhq34xrr8zuaqgv4ajf36vg2mmsruqt5m3lv",
+    satsPerByte: 1,
+    publicKey:
+      "039ce27aa7666731648421004ba943b90b8273e23a175d9c58e3ec2e643a9b01d1",
+    outputs: [
+      {
+        address: "tb1qatkgzm0hsk83ysqja5nq8ecdmtwl73zwurawww",
+        value: 600,
+      },
+    ],
+    network: "testnet",
+  });
+  console.log("Created Psbt: ", psbt);
+}
+
+const createPsbtButton = document.getElementById("create-psbt");
+if (createPsbtButton) {
+  createPsbtButton.addEventListener("click", handleCreatePsbtButtonClick);
 }
---
+++ packages/sdk/src/transactions/index.ts
@@ -0,0 +1,2 @@
+export * from "./psbt";
+export { PSBTBuilder } from "./PSBTBuilder";
