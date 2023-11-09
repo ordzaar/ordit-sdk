d9670bc759266bae483f16627e82d6dc75d0a1b7
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-11-09 18:49:36 +0800

    refactor(sdk): api types and add docs

--- packages/sdk/src/api/types.ts
+++ packages/sdk/src/api/types.ts
@@ -5,11 +5,34 @@ import type { Transaction, UTXO } from "../transactions/types";
 import type { RequireAtLeastOne } from "../utils/types";
 
 export interface GetUnspentsOptions {
+  /**
+   * Address to list unspent UTXOs for
+   */
   address: string;
+
+  /**
+   * Types of UTXOs
+   */
   type?: "all" | "spendable";
+
+  /**
+   * List of rarities that defines safe to spend threshold
+   */
   rarity?: Rarity[];
+
+  /**
+   * Sorting order
+   */
   sort?: "asc" | "desc";
+
+  /**
+   * Amount of items to retrieve in a single request
+   */
   limit?: number;
+
+  /**
+   * Fetch records in forward order starting from the given cursor
+   */
   next?: string | null;
 }
 
@@ -19,15 +42,34 @@ export interface GetUnspentsResponse {
   unspendableUTXOs: UTXO[];
 }
 
-export interface GetTxOptions {
+export interface GetTransactionOptions {
+  /**
+   * Transaction id to retrieve from the blockchain
+   */
   txId: string;
+
+  /**
+   * Include ordinal and inscription information in transaction outputs response
+   */
   ordinals?: boolean;
+
+  /**
+   * Include transaction hex in response
+   */
   hex?: boolean;
+
+  /**
+   * Include witness data in response
+   */
   witness?: boolean;
+
+  /**
+   * Decode inscription metadata into [valid URI components](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/decodeURIComponent)
+   */
   decodeMetadata?: boolean;
 }
 
-export interface FetchTxResponse {
+export interface GetTransactionResponse {
   tx: Transaction;
   rawTx?: BTCTransaction;
 }
@@ -50,22 +92,65 @@ export type GetInscriptionsOptions = RequireAtLeastOne<{
 };
 
 export interface GetInscriptionOptions {
+  /**
+   * Inscription id
+   */
   id: string;
+
+  /**
+   * Decode inscription metadata into [valid URI components](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/decodeURIComponent)
+   */
   decodeMetadata?: boolean;
 }
 
 export interface RelayOptions {
+  /**
+   * Raw transaction as hex string used to submit the transaction to the blockchain.
+   */
   hex: string;
+
+  /**
+   * Reject transactions whose fee rate is higher than the specified value, expressed in BTC/kB.
+   *
+   * Set to 0 to accept any fee rate.
+   */
   maxFeeRate?: number;
+
+  /**
+   * Performs some basic validation against our local UTXO data to verify that the transaction has valid inputs using our own internal tracking of unconfirmed spent states.
+   */
   validate?: boolean;
 }
 
 export interface GetSpendablesOptions {
+  /**
+   * Address to list spendable UTXOs for
+   */
   address: string;
+
+  /**
+   * Value to spend
+   */
   value: number;
+
+  /**
+   * Types of UTXOs
+   */
   type?: "all" | "spendable";
+
+  /**
+   * List of rarities that defines safe to spend threshold
+   */
   rarity?: Rarity[];
+
+  /**
+   * Filter
+   */
   filter?: string[];
+
+  /**
+   * Amount of items to retrieve in a single request
+   */
   limit?: number;
 }
 
--- packages/sdk/src/modules/BaseDatasource.ts
+++ packages/sdk/src/modules/BaseDatasource.ts
@@ -1,19 +1,18 @@
-import type { Transaction as BTCTransaction } from "bitcoinjs-lib";
-
 import type {
   GetBalanceOptions,
   GetInscriptionOptions,
   GetInscriptionsOptions,
   GetInscriptionUTXOOptions,
   GetSpendablesOptions,
-  GetTxOptions,
+  GetTransactionOptions,
+  GetTransactionResponse,
   GetUnspentsOptions,
   GetUnspentsResponse,
   RelayOptions,
 } from "../api/types";
 import type { Network } from "../config/types";
 import type { Inscription } from "../inscription/types";
-import type { Transaction, UTXO, UTXOLimited } from "../transactions/types";
+import type { UTXO, UTXOLimited } from "../transactions/types";
 
 export interface BaseDatasourceOptions {
   network: Network;
@@ -26,15 +25,35 @@ export abstract class BaseDatasource {
     this.network = network;
   }
 
+  /**
+   * Gets the total balance of available for an address.
+   *
+   * @param options List of options to modify the result provided.
+   */
   abstract getBalance({ address }: GetBalanceOptions): Promise<number>;
 
+  /**
+   * Gets an inscription by inscription id.
+   *
+   * @param options List of options to modify the result provided.
+   */
   abstract getInscription({
     id,
     decodeMetadata,
   }: GetInscriptionOptions): Promise<Inscription>;
 
+  /**
+   * Gets an inscription UTXO by inscription id.
+   *
+   * @param options List of options to modify the result provided.
+   */
   abstract getInscriptionUTXO({ id }: GetInscriptionUTXOOptions): Promise<UTXO>;
 
+  /**
+   * Gets a list of indexed inscriptions.
+   *
+   * @param options List of options to modify the result provided.
+   */
   abstract getInscriptions({
     creator,
     owner,
@@ -47,6 +66,11 @@ export abstract class BaseDatasource {
     decodeMetadata,
   }: GetInscriptionsOptions): Promise<Inscription[]>;
 
+  /**
+   * Gets a list of all spendable utxos under the given address.
+   *
+   * @param options List of options to modify the result provided.
+   */
   abstract getSpendables({
     address,
     value,
@@ -56,14 +80,24 @@ export abstract class BaseDatasource {
     limit,
   }: GetSpendablesOptions): Promise<UTXOLimited[]>;
 
+  /**
+   * Gets a transaction from the blockchain with optional projection and ordinal details.
+   *
+   * @param options List of options to modify the result provided.
+   */
   abstract getTransaction({
     txId,
     ordinals,
     hex,
     witness,
     decodeMetadata,
-  }: GetTxOptions): Promise<{ tx: Transaction; rawTx?: BTCTransaction }>;
+  }: GetTransactionOptions): Promise<GetTransactionResponse>;
 
+  /**
+   * Gets a list of all unspent utxos under the given address.
+   *
+   * @param options List of options to modify the result provided.
+   */
   abstract getUnspents({
     address,
     type,
@@ -73,5 +107,14 @@ export abstract class BaseDatasource {
     next,
   }: GetUnspentsOptions): Promise<GetUnspentsResponse>;
 
+  /**
+   * Submits a raw transaction (serialized, hex-encoded) to local node and network.
+   *
+   * Note that the transaction will be sent unconditionally to all peers, so using this for manual rebroadcast may degrade privacy by leaking the transaction’s origin, as nodes will normally not rebroadcast non-wallet transactions already in their mempool.
+   *
+   * See [sendrawtransaction](https://developer.bitcoin.org/reference/rpc/sendrawtransaction.html) for more information on the behavior of this method.
+   *
+   * @param options List of options to modify the result provided.
+   */
   abstract relay({ hex, maxFeeRate, validate }: RelayOptions): Promise<string>;
 }
