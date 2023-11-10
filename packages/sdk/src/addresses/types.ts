fd59a6d12d3198ecec37ef715ec018f80fa2fb1b
Nanosync <10258208+Nanosync@users.noreply.github.com>
2023-11-10 15:44:53 +0800

    fix(sdk): address type

--- packages/sdk/src/addresses/types.ts
+++ packages/sdk/src/addresses/types.ts
@@ -3,8 +3,23 @@ export type AddressType = "p2pkh" | "p2sh" | "p2wpkh" | "p2tr";
 export type AddressFormat = "legacy" | "p2sh-p2wpkh" | "segwit" | "taproot";
 
 export type Address = {
+  /**
+   * Address
+   */
   address: string;
-  xKey?: string;
+
+  /**
+   * Address format
+   */
   format: AddressFormat;
+
+  /**
+   * Public key
+   */
   publicKey: string;
+
+  /**
+   * x-coordinate of the public key, when taproot is used
+   */
+  xKey?: string;
 };
