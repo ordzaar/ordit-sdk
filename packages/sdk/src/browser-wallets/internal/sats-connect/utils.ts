36aed162df5b8f0dd96e5cb1d033a4af53273c62
KevinK <31565174+kevzzsk@users.noreply.github.com>
2024-05-15 18:14:53 +0800

    fix(sdk): fix xverse returning compressed pubkey instead of xOnly (#182)

--- packages/sdk/src/browser-wallets/internal/sats-connect/utils.ts
+++ packages/sdk/src/browser-wallets/internal/sats-connect/utils.ts
@@ -1,3 +1,9 @@
 export function fromXOnlyToFullPubkey(xOnly: string): string {
+  const xOnlyBuffer = Buffer.from(xOnly, "hex");
+  // if xOnly is 33 bytes, it's already a full pubkey
+  if (xOnlyBuffer.length === 33) {
+    return xOnly;
+  }
+  // source: https://medium.com/blockstream/reducing-bitcoin-transaction-sizes-with-x-only-pubkeys-f86476af05d7
   return `02${xOnly}`; // prepend y-coord/tie-breaker to x-only
 }
